c7f75c9da17daddebe3591c34cbe964977bc1ceb
Orim Dominic <orimdominic@gmail.com>
2022-02-11 17:43:55 +0100

    chore: remove dead code

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  if (process.env.NODE_ENV === 'test') {
-    // to trigger MongoMemoryServer to shutdown on triggering app.close per test
-    app.enableShutdownHooks();
-  }
   await app.listen(3000);
 }
 bootstrap();
