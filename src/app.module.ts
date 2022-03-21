c5ebccf53b615be096c9ebaa03af4b0cffb46537
Orim Dominic <orimdominic@gmail.com>
2022-03-21 16:30:40 +0100

    revert to using path.join

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,31 +6,34 @@ import { UsersModule } from './users/users.module';
 import { User } from './entities/user.entity';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { getTestDb } from './test-db';
+import { ItemsModule } from './items/items.module';
+import { join } from 'path';
 let mongod: MongoMemoryServer;
 
 @Module({
   imports: [
     process.env.NODE_ENV === 'test'
       ? TypeOrmModule.forRootAsync({
-          useFactory: async () => {
-            mongod = await getTestDb();
-            return {
-              url: mongod.getUri(),
-              type: 'mongodb',
-              host: 'localhost',
-              database: 'mongo-typeorm-e2e',
-              entities: [User],
-            };
-          },
-        })
+        useFactory: async () => {
+          mongod = await getTestDb();
+          return {
+            url: mongod.getUri(),
+            type: 'mongodb',
+            host: 'localhost',
+            database: 'mongo-typeorm-e2e',
+            entities: [join(__dirname + '/**/*.entity{.ts,.js}')],
+          };
+        },
+      })
       : TypeOrmModule.forRoot({
-          type: 'mongodb',
-          host: 'localhost',
-          port: 27017,
-          database: 'mongo-typeorm-e2e',
-          entities: [User],
-        }),
+        type: 'mongodb',
+        host: 'localhost',
+        port: 27017,
+        database: 'mongo-typeorm-e2e',
+        entities: [join(__dirname + '/**/*.entity{.ts,.js}')],
+      }),
     UsersModule,
+    ItemsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
